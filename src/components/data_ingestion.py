cdd14871da8227e471330c11c7966105773fecb7
Ashita Gupta <ashita9305@gmail.com>
2025-06-02 11:26:08 +0530

    Data Transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 class DataIngestionConfig:
     ##folder,filename
     train_data_path:str=os.path.join('artifacts','train.csv')
