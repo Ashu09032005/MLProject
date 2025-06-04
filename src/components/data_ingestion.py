ddcfd554a81bc80fd45698873f0f6d36d5004814
Ashita Gupta <ashita9305@gmail.com>
2025-06-04 21:33:20 +0530

    added features

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,9 +10,12 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 from src.components.model_train import  ModelTrainer,ModelTrainerConfig
-
+##basically we have read the data made 3 files and stored in artifacts folder
 
 @dataclass
+##This defines where files will be saved.(in artifacts folder)
+
+
 class DataIngestionConfig:
     
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -28,13 +31,13 @@ class DataIngestion:
         try:
             df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
-            
+            ## if artifacts folder dne then to make it 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            
+            ## makes raw data file
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info('Train test split initiated')
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            
+            ##csv files
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             logging.info('Ingestion of the data is completed')
